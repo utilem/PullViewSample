74b15f71d536ff9aabec1a449209c166d45c6b46
Uwe Tilemann <n4@even-u.com>
2022-09-25 05:52:04 +0200

    Initial Commit

---
+++ PopoverSample/PopoverSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopoverSampleApp.swift
+//  PopoverSample
+//
+//  Created by Uwe Tilemann on 25.09.22.
+//
+
+import SwiftUI
+
+@main
+struct PopoverSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
